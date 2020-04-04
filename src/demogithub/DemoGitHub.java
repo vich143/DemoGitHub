76b7b18d58fd169832aa86926e92aeeca53eb299
vich143 <hungvi122@gmail.com>
2020-04-04 10:41:05 +0700

    init project

---
+++ src/demogithub/DemoGitHub.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogithub;
+
+/**
+ *
+ * @author vich
+ */
+public class DemoGitHub {
+
+    /**
+     * @param args the command line
+     * arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello World");
+    }
+
+}
